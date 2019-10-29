e9e267815f044b3c9756ae009c7ca9acee8d19f5
Alexander Dutton <alex.dutton@mysociety.org>
2019-10-29 09:08:29 +0000

    Harborough

--- polling_stations/apps/data_collection/management/commands/import_harborough.py
+++ polling_stations/apps/data_collection/management/commands/import_harborough.py
@@ -4,13 +4,27 @@ from data_collection.management.commands import BaseXpressDemocracyClubCsvImport
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "E07000131"
     addresses_name = (
-        "local.2019-05-02/Version 1/Democracy_Club__02May2019 Harborough DC.tsv"
+        "parl.maybe/Version 1/harborough-Democracy_Club__15October2019.tsv"
     )
     stations_name = (
-        "local.2019-05-02/Version 1/Democracy_Club__02May2019 Harborough DC.tsv"
+        "parl.maybe/Version 1/harborough-Democracy_Club__15October2019.tsv"
     )
-    elections = ["local.2019-05-02", "europarl.2019-05-23"]
+    elections = ["parl.maybe"]
     csv_delimiter = "\t"
+
+    def station_record_to_dict(self, record):
+        station_uprns = {
+            '5110': '100032073908',  # Broughton Astley Village Hall (Main Hall)
+            '4924': '200003742088',  # Meadowdale Primary School
+            # '5029': '',  # Bruntingthorpe Vilalge Hall
+        }
+        if record.polling_place_id in station_uprns:
+            record = record._replace(polling_place_uprn=station_uprns[record.polling_place_id])
+
+        if record.polling_place_id == '4949':
+            record = record._replace(polling_place_postcode='LE7 9FL')
+
+        return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
         rec = super().address_record_to_dict(record)
@@ -18,6 +32,9 @@ class Command(BaseXpressDemocracyClubCsvImporter):
 
         if uprn == "200003741884":
             rec["postcode"] = "LE14 2QY"
+
+        if uprn == '10093549892':
+            return None  # UPRN/address confusion; removing means both affected postcodes return correct results
 
         if (
             record.addressline1.strip() == "69 Main Street"
@@ -33,6 +50,8 @@ class Command(BaseXpressDemocracyClubCsvImporter):
             "200003741317",  # LE79DE -> LE79DP : Robin A Tiptoe Farm, Loddington Road, Tilton on the Hill, Leicester
             "200003742237",  # LE79XE -> LE79XB : Ash Tree Cottage, Launde Road, Loddington, Leicester
             "100030477785",  # LE96PU -> LE96PW : 102 Station Road, Broughton Astley, Leics
+            "10093549578",  # LE169FR -> LE176PU : 19 Marsh Drive, Husbands Bosworth, Leics
+            "10093551803",  # LE79EX -> LE79LX : 1 Stacey Avenue, Houghton on the Hill, Leicester
         ]:
             rec["accept_suggestion"] = True
 
@@ -46,7 +65,7 @@ class Command(BaseXpressDemocracyClubCsvImporter):
             "100030480043",  # LE174RU -> LE174RX : Toll Gate Cottage, Bitteswell Road, Lutterworth, Leics
             "10034458557",  # LE175LE -> LE174LE : The Milking Parlour Boston Lodge, Lutterworth Road, Gilmorton, Lutterworth, Leics
             "200003744797",  # LE175PL -> LE175RZ : Ewe Cottage Gilmorton Lodge, Kimcote Road, Gilmorton, Lutterworth, Leics
-            "100030493741",  # LE167TT -> LE167TX : Birchtree Farm, Welham Road, Thorpe Langton, Leics
+        #     "100030493741",  # LE167TT -> LE167TX : Birchtree Farm, Welham Road, Thorpe Langton, Leics
             "200003742100",  # LE174LH -> LE174LR : The Mere, Mere Road, Bitteswell, Lutterworth, Leics
             "200003741377",  # LE79XL -> LE79XJ : 3 Fiddlers Green, Uppingham Road, East Norton, Leicester
             "200003741379",  # LE79XL -> LE79XJ : 2 Fiddlers Green, Uppingham Road, East Norton, Leicester
