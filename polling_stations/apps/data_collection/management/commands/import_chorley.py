de34e90e90c4443bf7aa6954ca7078077f9cd62b
Alexander Dutton <alex.dutton@mysociety.org>
2019-10-29 09:53:12 +0000

    Chorley

--- polling_stations/apps/data_collection/management/commands/import_chorley.py
+++ polling_stations/apps/data_collection/management/commands/import_chorley.py
@@ -4,30 +4,39 @@ from data_collection.management.commands import BaseHalaroseCsvImporter
 class Command(BaseHalaroseCsvImporter):
     council_id = "E07000118"
     addresses_name = (
-        "europarl.2019-05-23/Version 1/polling_station_export-2019-05-13chor.csv"
+        "parl.maybe/Version 1/chorley-polling_station_export-2019-09-16.csv"
     )
     stations_name = (
-        "europarl.2019-05-23/Version 1/polling_station_export-2019-05-13chor.csv"
+        "parl.maybe/Version 1/chorley-polling_station_export-2019-09-16.csv"
     )
-    elections = ["europarl.2019-05-23"]
+    elections = ["parl.maybe"]
+    allow_station_point_from_postcode = False
 
     def address_record_to_dict(self, record):
         rec = super().address_record_to_dict(record)
         uprn = record.uprn.strip().lstrip("0")
 
-        if record.housepostcode == "PR6 7ED":
-            return None
+        # # This is a hotel which isn't in AddressBase, but which doesn't cause any harm if the record is left in.
+        # # Leaving this as a comment in case it becomes useful again in future.
+        # if record.housepostcode == "PR6 7ED":
+        #     return None
 
         if uprn in [
+            "100010362601",  # PR69NN -> PR69NW : 14 BABYLON LANE, ADLINGTON, CHORLEY, LANCASHIRE
+            "100010367883",  # PR69LB -> PR69LJ : 6 CHORLEY ROAD, HEATH CHARNOCK, CHORLEY, LANCASHIRE
+            "200004063697",  # PR68NT -> PR68NS : JACK GREEN COTTAGE, ORAM ROAD, BRINDLE, CHORLEY, LANCASHIRE
+            "200004063671",  # PR50SD -> PR76ND : BRIDGE END COTTAGE, VALLEY ROAD, HOGHTON, PRESTON, LANCASHIRE
+            "100012384782",  # PR60HT -> PR60HP : ST PETERS VICARAGE, HARPERS LANE, CHORLEY, LANCASHIRE
             "200004070947",  # PR72QL -> PR73QL : 2 PLYMOUTH COTTAGES, BIRKACRE ROAD
             "10002076206",  # PR67LB -> PR67LS : HILLSIDE COTTAGE, 1 NAYLORS FOLD HILL TOP LANE
             "100010385659",  # PR75DB -> PR75DE : THE BUNGALOW SPENDMORE LANE, COPPULL
             "10070372269",  # PR69AT -> PR269AT : SNOWDROP COTTAGE BANK HALL DRIVE, BRETHERTON
         ]:
             rec["accept_suggestion"] = True
-
-        # just bin the UPRNs on these ones
+        #
+        # # just bin the UPRNs on these ones
         if uprn in [
+            "10091495601",  # PR69RF -> PR60QF : GROVE FARM, RAILWAY ROAD, ADLINGTON, CHORLEY, LANCASHIRE
             "100010391254",  # PR75TW -> L402QN : 41 NEW STREET, ECCLESTON
             "100010391256",  # PR75TW -> L402QN : 43 NEW STREET, ECCLESTON
             "10070371313",  # L402QN -> PR75TW : 41 NEW STREET, MAWDESLEY, ORMSKIRK
