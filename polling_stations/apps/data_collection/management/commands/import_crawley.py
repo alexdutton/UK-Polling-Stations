ec3362779ad6d14fb8f55d1dc3b995b4f56ed8b6
Alexander Dutton <alex.dutton@mysociety.org>
2019-10-29 09:08:06 +0000

    Crawley

--- polling_stations/apps/data_collection/management/commands/import_crawley.py
+++ polling_stations/apps/data_collection/management/commands/import_crawley.py
@@ -3,17 +3,28 @@ from data_collection.management.commands import BaseXpressDemocracyClubCsvImport
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "E07000226"
-    addresses_name = "local.2019-05-02/Version 2/Democracy_Club__02May2019.tsv"
-    stations_name = "local.2019-05-02/Version 2/Democracy_Club__02May2019.tsv"
-    elections = ["local.2019-05-02", "europarl.2019-05-23"]
+    addresses_name = "parl.maybe/Version 1/crawley-Democracy_Club__15October2019.tsv"
+    stations_name = "parl.maybe/Version 1/crawley-Democracy_Club__15October2019.tsv"
+    elections = ["parl.maybe"]
     csv_delimiter = "\t"
+
+    def station_record_to_dict(self, record):
+        if record.polling_place_id == '795':  # Southgate West Community Centre
+            record = record._replace(polling_place_uprn='100062475395', polling_place_easting='', polling_place_northing='')
+        if record.polling_place_id == '789':  # Wakehams Green Community Centre
+            record = record._replace(polling_place_uprn='200001230277', polling_place_easting='', polling_place_northing='',
+                                     polling_place_postcode='RH10 3NU')
+        if record.polling_place_id == '774':  # Furnace Green Community Centre
+            record = record._replace(polling_place_uprn='100062615172', polling_place_easting='', polling_place_northing='',
+                                     polling_place_postcode='RH10 6QZ')
+        return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
         rec = super().address_record_to_dict(record)
         uprn = record.property_urn.strip().lstrip("0")
 
-        if record.addressline6 == "RH10 3HW":
-            rec["postcode"] = "RH10 3GW"
+        # if record.addressline6 == "RH10 3HW":
+        #     rec["postcode"] = "RH10 3GW"
 
         if uprn in [
             "10024122201"  # RH110EA -> RH110AE : 50A Ifield Drive, Ifield, Crawley
@@ -21,12 +32,12 @@ class Command(BaseXpressDemocracyClubCsvImporter):
             rec["accept_suggestion"] = True
 
         return rec
-
-    def station_record_to_dict(self, record):
-        if record.polling_place_id == "675":
-            record = record._replace(polling_place_easting="526564")
-            record = record._replace(polling_place_northing="135576")
-        if record.polling_place_id == "692":
-            record = record._replace(polling_place_easting="528408")
-            record = record._replace(polling_place_northing="135808")
-        return super().station_record_to_dict(record)
+    #
+    # def station_record_to_dict(self, record):
+    #     if record.polling_place_id == "675":
+    #         record = record._replace(polling_place_easting="526564")
+    #         record = record._replace(polling_place_northing="135576")
+    #     if record.polling_place_id == "692":
+    #         record = record._replace(polling_place_easting="528408")
+    #         record = record._replace(polling_place_northing="135808")
+    #     return super().station_record_to_dict(record)
